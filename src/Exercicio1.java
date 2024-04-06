bd9407a87952850dd4edd9dd819f232a3a7320d2
PedroDovigo <pedrodpais2@gmail.com>
2024-04-06 09:00:47 -0300

    Exercicio 1 feito

--- src/Exercicio1.java
+++ src/Exercicio1.java
@@ -1,3 +1,15 @@
-public class Exercicio1 {
+import java.util.Scanner;
 
+public class Exercicio1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Digite o primeiro valor: ");
+        float num1 = scanner.nextFloat();
+
+        System.out.println("Digite o segundo valor: ");
+        float num2 = scanner.nextFloat();
+
+        String resultado = num1 > num2 ? "O primeiro valor é maior." : "O segundo valor é maior.";
+        System.out.println(resultado);
+    }
 }
