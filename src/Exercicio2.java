881c31bae754dfcf6e684d72337e929a417839a8
PedroDovigo <pedrodpais2@gmail.com>
2024-04-06 09:22:54 -0300

    Exercicio 2 feito

--- src/Exercicio2.java
+++ src/Exercicio2.java
@@ -1,2 +1,22 @@
+import java.util.Scanner;
+
 public class Exercicio2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+        System.out.println("Olá, vamos descobrir se você pode votar?");
+        System.out.println("Digite seu ano de nascimento: ");
+        int anoNascimento = scanner.nextInt();
+
+        int podeVotar = 2024 - anoNascimento;
+
+        if (podeVotar >= 16) {
+            System.out.println("Parabéns, você já pode votar!");
+        } else {
+            int faltaIdade = 16 - podeVotar;
+            System.out.println("Infelizmente você ainda não consegue votar. Faltam " + faltaIdade + " anos para você poder votar!");
+        }
+
+    }
 }
